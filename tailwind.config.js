5963673927f10d596e19192845a2d05bd9d86253
Taiwo-Ayomide <taiwoayomide202@gmail.com>
2024-12-05 22:17:32 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// /** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Add this line to ensure Tailwind can find your React components
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
